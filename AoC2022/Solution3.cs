73996540b3c33e071f0c6b7ed30d97313727f197
KoenPlasmans <koen.plasmans@lansweeper.com>
2021-12-02 09:21:51 +0100

    done

---
+++ AoC2022/Solution3.cs
@@ -0,0 +1,33 @@
+﻿namespace AoC2022
+{
+    public class Solution3 : IProvideSolution
+    {
+        public string GetSolution()
+        {
+            var lines = new FileInfo("Input/solution2.txt").OpenText().ReadToEnd().Split("\n");
+            return BaseAlgorithm(lines);
+        }
+
+        public string BaseAlgorithm(string[] lines)
+        {
+            var depth = 0;
+            var hor = 0;
+            foreach(var line in lines)
+            {
+                if (line.StartsWith("forward"))
+                {
+                    hor += int.Parse(line.Split(" ")[1]);
+                }
+                else if (line.StartsWith("up"))
+                {
+                    depth -= int.Parse(line.Split(" ")[1]);
+                }
+                else if (line.StartsWith("down"))
+                {
+                    depth += int.Parse(line.Split(" ")[1]);
+                }
+            }
+            return (depth * hor).ToString();
+        }
+    }
+}
---
+++ AoC2022/Solution4.cs
@@ -0,0 +1,36 @@
+﻿namespace AoC2022
+{
+    public class Solution4 : IProvideSolution
+    {
+        public string GetSolution()
+        {
+            var lines = new FileInfo("Input/solution2.txt").OpenText().ReadToEnd().Split("\n");
+            return BaseAlgorithm(lines);
+        }
+
+        public string BaseAlgorithm(string[] lines)
+        {
+            var aim = 0;
+            var depth = 0;
+            var hor = 0;
+            foreach(var line in lines)
+            {
+                if (line.StartsWith("forward"))
+                {
+                    var val = int.Parse(line.Split(" ")[1]);
+                    hor += int.Parse(line.Split(" ")[1]);
+                    depth += val * aim;
+                }
+                else if (line.StartsWith("up"))
+                {
+                    aim -= int.Parse(line.Split(" ")[1]);
+                }
+                else if (line.StartsWith("down"))
+                {
+                    aim += int.Parse(line.Split(" ")[1]);
+                }
+            }
+            return (depth * hor).ToString();
+        }
+    }
+}
