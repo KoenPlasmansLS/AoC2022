96024d8b3cf20432ac783249ccf698af5d6c669a
KoenPlasmans <koen.plasmans@lansweeper.com>
2021-12-10 09:26:18 +0100

    day 10

---
+++ AoC2022/Solution19.cs
@@ -0,0 +1,45 @@
+﻿namespace AoC2022
+{
+    public class Solution19 : IProvideSolution
+    {
+        public string GetSolution()
+        {
+            var lines = new FileInfo("Input/solution10.txt").OpenText().ReadToEnd().Split("\n");
+            return BaseAlgorithm(lines);
+        }
+
+        private string BaseAlgorithm(string[] lines)
+        {
+            var sum = 0;
+            foreach(var line in lines)
+            {
+                var lineToInvestigate = line.Trim();
+                var newLine = lineToInvestigate;
+                do
+                {
+                    lineToInvestigate = newLine;
+                    newLine = lineToInvestigate
+                        .Replace("<>", string.Empty)
+                        .Replace("[]", string.Empty)
+                        .Replace("{}", string.Empty)
+                        .Replace("()", string.Empty);
+
+                }
+                while (newLine != string.Empty && newLine.Length != lineToInvestigate.Length);
+                if (newLine != string.Empty)
+                {
+                    var incompleteOrCorrupted = newLine.Replace("(", string.Empty).Replace("[", string.Empty).Replace("{", string.Empty).Replace("<", string.Empty);
+                    if (incompleteOrCorrupted != string.Empty)
+                    {
+                        var corruptedChar = incompleteOrCorrupted[0];
+                        if (corruptedChar == '>') sum += 25137;
+                        if (corruptedChar == ')') sum += 3;
+                        if (corruptedChar == ']') sum += 57;
+                        if (corruptedChar == '}') sum += 1197;
+                    }
+                }
+            }
+            return sum.ToString();
+        }
+    }
+}
---
+++ AoC2022/Solution20.cs
@@ -0,0 +1,52 @@
+﻿namespace AoC2022
+{
+    public class Solution20 : IProvideSolution
+    {
+        public string GetSolution()
+        {
+            var lines = new FileInfo("Input/solution10.txt").OpenText().ReadToEnd().Split("\n");
+            return BaseAlgorithm(lines);
+        }
+
+        private string BaseAlgorithm(string[] lines)
+        {
+            var scores = new List<long>();
+            foreach(var line in lines)
+            {
+                var lineToInvestigate = line.Trim();
+                var newLine = lineToInvestigate;
+                do
+                {
+                    lineToInvestigate = newLine;
+                    newLine = lineToInvestigate
+                        .Replace("<>", string.Empty)
+                        .Replace("[]", string.Empty)
+                        .Replace("{}", string.Empty)
+                        .Replace("()", string.Empty);
+
+                }
+                while (newLine != string.Empty && newLine.Length != lineToInvestigate.Length);
+                if (newLine != string.Empty)
+                {
+                    var incompleteOrCorrupted = newLine.Replace("(", string.Empty).Replace("[", string.Empty).Replace("{", string.Empty).Replace("<", string.Empty);
+                    if (incompleteOrCorrupted == string.Empty)
+                    {
+                        var reverse = newLine.Reverse();
+                        long score = 0;
+                        foreach(var chr in reverse)
+                        {
+                            score *= 5;
+                            if (chr == '(') score += 1;
+                            if (chr == '[') score += 2;
+                            if (chr == '{') score += 3;
+                            if (chr == '<') score += 4;
+                        }
+                        scores.Add(score);
+                    }
+                }
+            }
+            scores.Sort();
+            return scores[scores.Count / 2].ToString();
+        }
+    }
+}
