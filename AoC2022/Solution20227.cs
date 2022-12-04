2c129a258b7101ace0f3ccb23b6f1b41baf56d1b
Koen Plasmans <koen.plasmans@lansweeper.com>
2022-12-04 21:24:41 +0100

    day 4

---
+++ AoC2022/Solution20227.cs
@@ -0,0 +1,28 @@
+﻿namespace AoC2022
+{
+    public class Solution20227 : IProvideSolution
+    {
+        public virtual string GetSolution()
+        {
+            var lines = new FileInfo("Input/2022solution4.txt").OpenText().ReadToEnd().Split("\n");
+            return BaseAlgorithm(lines);
+        }
+
+        protected static string BaseAlgorithm(string[] lines)
+        {
+            var sum = 0;
+            for (var i = 0; i < lines.Count(); i++)
+            {
+                var splt = lines[i].Split(",");
+                var rng = new Range(int.Parse(splt[0].Split("-")[0]), int.Parse(splt[0].Split("-")[1]));
+                var rng2 = new Range(int.Parse(splt[1].Split("-")[0]), int.Parse(splt[1].Split("-")[1]));
+                if ((rng.Start.Value >= rng2.Start.Value && rng.End.Value <= rng2.End.Value) || (rng2.Start.Value >= rng.Start.Value && rng2.End.Value <= rng.End.Value)) 
+                { 
+                    sum += 1; 
+                }
+            }
+            
+            return sum.ToString();
+        }
+    }
+}
