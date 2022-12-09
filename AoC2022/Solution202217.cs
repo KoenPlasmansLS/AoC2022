e32dea5818c0cc03f7fc88f3f762547d103b39ca
Koen Plasmans <koen.plasmans@lansweeper.com>
2022-12-09 09:13:14 +0100

    day 9

---
+++ AoC2022/Solution202217.cs
@@ -0,0 +1,70 @@
+﻿namespace AoC2022
+{
+    public class Solution202217 : IProvideSolution
+    {
+        public virtual string GetSolution()
+        {
+            var lines = new FileInfo("Input/2022solution9.txt").OpenText().ReadToEnd().Split("\n");
+            return BaseAlgorithm(lines);
+        }
+
+        protected static string BaseAlgorithm(string[] lines)
+        {
+            var xPosT = 0;
+            var yPosT = 0;
+            var xPosH = 0;
+            var yPosH = 0;
+            var lst = new List<(int, int)>();
+            lst.Add((xPosT, yPosT));
+            foreach (var line in lines)
+            {
+                var pos = line.Trim().Split(" ")[0];
+                var times = int.Parse(line.Trim().Split(" ")[1]);
+                for (var i = 0; i < times; i++)
+                {
+                    if (pos == "R")
+                    {
+                        xPosH++;
+                    }
+                    if (pos == "L")
+                    {
+                        xPosH--;
+                    }
+                    if (pos == "U")
+                    {
+                        yPosH++;
+                    }
+                    if (pos == "D")
+                    {
+                        yPosH--;
+                    }
+                    if (xPosH - xPosT == 2)
+                    {
+                        xPosT++;
+                        yPosT = yPosH;
+                    }
+                    if (xPosT - xPosH == 2)
+                    {
+                        xPosT--;
+                        yPosT = yPosH;
+                    }
+                    if (yPosT - yPosH == 2)
+                    {
+                        yPosT--;
+                        xPosT = xPosH;
+                    }
+                    if (yPosH - yPosT == 2)
+                    {
+                        yPosT++;
+                        xPosT = xPosH;
+                    }
+                    if (!lst.Contains((xPosT, yPosT)))
+                    {
+                        lst.Add((xPosT, yPosT));
+                    }
+                }
+            }
+            return lst.Count.ToString();
+        }
+    }
+}
---
+++ AoC2022/solution202218.cs
@@ -0,0 +1,84 @@
+﻿namespace AoC2022
+{
+    public class Solution202218 : IProvideSolution
+    {
+        public virtual string GetSolution()
+        {
+            var lines = new FileInfo("Input/2022solution9.txt").OpenText().ReadToEnd().Split("\n");
+            return BaseAlgorithm(lines);
+        }
+
+        protected static string BaseAlgorithm(string[] lines)
+        {
+            var posses = new (int, int)[10];
+            var lst = new List<(int, int)>();
+            lst.Add((posses[9].Item1, posses[9].Item2));
+            foreach (var line in lines)
+            {
+                var pos = line.Trim().Split(" ")[0];
+                var times = int.Parse(line.Trim().Split(" ")[1]);
+                for (var i = 0; i < times; i++)
+                {
+                    if (pos == "R")
+                    {
+                        posses[0].Item1++;
+                    }
+                    if (pos == "L")
+                    {
+                        posses[0].Item1--;
+                    }
+                    if (pos == "U")
+                    {
+                        posses[0].Item2++;
+                    }
+                    if (pos == "D")
+                    {
+                        posses[0].Item2--;
+                    }
+                    for (var j = 0; j < 9; j++)
+                    {
+                        var movedX = false;
+                        var movedY = false;
+                        if (posses[j].Item1 - posses[j + 1].Item1 == 2)
+                        {
+                            posses[j + 1].Item1++;
+                            movedX = true;
+                            //posses[j + 1].Item2 = posses[j].Item2;
+                        }
+                        if (posses[j + 1].Item1 - posses[j].Item1 == 2)
+                        {
+                            posses[j + 1].Item1--;
+                            movedX = true;
+                            //posses[j + 1].Item2 = posses[j].Item2;
+                        }
+                        if (posses[j].Item2 - posses[j + 1].Item2 == 2)
+                        {
+                            posses[j + 1].Item2++;
+                            movedY = true;
+                            //posses[j + 1].Item1 = posses[j].Item1;
+                        }
+                        if (posses[j + 1].Item2 - posses[j].Item2 == 2)
+                        {
+                            posses[j + 1].Item2--;
+                            movedY = true;
+                            //posses[j + 1].Item1 = posses[j].Item1;
+                        }
+                        if (movedX && !movedY)
+                        {
+                            posses[j + 1].Item2 = posses[j].Item2;
+                        }
+                        if (movedY && !movedX)
+                        {
+                            posses[j + 1].Item1 = posses[j].Item1;
+                        }
+                    }
+                    if (!lst.Contains((posses[9].Item1, posses[9].Item2)))
+                    {
+                        lst.Add((posses[9].Item1, posses[9].Item2));
+                    }
+                }
+            }
+            return lst.Count.ToString();
+        }
+    }
+}
